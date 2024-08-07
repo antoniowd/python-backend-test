d2391b41a2b15bb90ac3d7bc6821bbddd5c39e05
Antonio Martin <tonitim13@gmail.com>
2024-08-07 09:43:26 -0300

    add main endpoints

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./storage.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./storage.sqlite3"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
