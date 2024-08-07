7ba61b85d272edc82de37aa04ce895d496ccd1be
Antonio Martin <tonitim13@gmail.com>
2024-08-07 08:38:04 -0300

    add models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Profile(Base):
+    __tablename__ = 'profiles'
+
+    id = Column(Integer, primary_key=True, index=True)
+    img = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    phone = Column(String)
+    address = Column(String)
+    city = Column(String)
+    state = Column(String)
+    zipcode = Column(String)
+    available = Column(Boolean, default=True)
